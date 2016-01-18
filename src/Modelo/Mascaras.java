fd93441aab97c6df38f842044d1216587c9ca40a
Alfonso Arcos <Alfonso Arcos@Alfonso.nervion.local>
2016-01-18 09:53:45 +0100

    mascaras del programa añadidas

---
+++ src/Modelo/Mascaras.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Alfonso Arcos
+ */
+public class Mascaras extends javax.swing.JFrame {
+
+    public void mascaraDNI(final JTextField a) {
+        for (int i = 0; i < a.getKeyListeners().length; i++) {
+            a.removeKeyListener(a.getKeyListeners()[i]);
+        }
+
+        a.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if (a.getText().length() < 8) {
+                    if (!Character.isDigit(c)) {
+                        getToolkit().beep();
+                        e.consume();
+                    }
+                } else if (a.getText().length() == 8) {
+                    if (Character.isDigit(c)) {
+                        getToolkit().beep();
+                        e.consume();
+                    }
+                } else {
+                    getToolkit().beep();
+                    e.consume();
+                }
+            }
+        });
+    }
+
+    public void mascaraString(final JTextField a, final int tam) {
+        for (int i = 0; i < a.getKeyListeners().length; i++) {
+            a.removeKeyListener(a.getKeyListeners()[i]);
+        }
+
+        a.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if (!(a.getText().length() < tam && !Character.isDigit(c))) {
+                    getToolkit().beep();
+                    e.consume();
+                }
+            }
+        });
+    }
+
+    public void mascaraStringNum(final JTextField a, final int tam) {
+        for (int i = 0; i < a.getKeyListeners().length; i++) {
+            a.removeKeyListener(a.getKeyListeners()[i]);
+        }
+
+        a.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if (!(a.getText().length() < tam)) {
+                    getToolkit().beep();
+                    e.consume();
+                }
+            }
+        });
+    }
+
+    public void mascaraInt(final JTextField a, final int tam) {
+        for (int i = 0; i < a.getKeyListeners().length; i++) {
+            a.removeKeyListener(a.getKeyListeners()[i]);
+        }
+
+        a.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if (!Character.isDigit(c) || a.getText().length() > tam - 1) {
+                    getToolkit().beep();
+                    e.consume();
+                }
+            }
+        });
+    }
+
+    public void mascaraDouble(final JTextField a, final int tam) {
+        for (int i = 0; i < a.getKeyListeners().length; i++) {
+            a.removeKeyListener(a.getKeyListeners()[i]);
+        }
+
+        a.addKeyListener(new KeyAdapter() {
+
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if ((!Character.isDigit(c) || a.getText().length() > tam - 1) && (c != '.' || a.getText().contains("."))) {
+                    getToolkit().beep();
+                    e.consume();
+                }
+            }
+        });
+    }
+}
