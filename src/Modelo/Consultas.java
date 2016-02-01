c46e920ba4e0477d9b1c261d7862227b08e078eb
jairo <jairo@192.168.50.134>
2016-02-01 19:32:59 +0100

    .

--- src/Modelo/Consultas.java
+++ src/Modelo/Consultas.java
@@ -3,9 +3,7 @@ package Modelo;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Date;
 import javax.swing.table.DefaultTableModel;
