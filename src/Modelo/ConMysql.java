bcbc0585e05329a62d8d498d9e45d17c22bfca76
Denryu Kurai Seishi <Denryu Kurai Seishi@DRAGON-2A9E3B7F.nervion.local>
2016-02-17 11:20:34 +0100

    - Cambiada ubicacion de la base de datos.

--- src/Modelo/ConMysql.java
+++ src/Modelo/ConMysql.java
@@ -6,6 +6,8 @@ import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 // @author Alfonso Ghost
+// @autor Denryu Kurai Seishi
+
 public class ConMysql {
 
     private Connection conexion = null;
@@ -17,7 +19,10 @@ public class ConMysql {
             // conexion = DriverManager.getConnection("jdbc:mysql://192.168.48.2:3306/dam3_Taller", "dam3_grupo", "grupo");
 
             // IP DESDE CASA
-            conexion = DriverManager.getConnection("jdbc:mysql://88.26.202.99:3306/dam3_Taller", "dam3_grupo", "grupo");
+            // conexion = DriverManager.getConnection("jdbc:mysql://88.26.202.99:3306/dam3_Taller", "dam3_grupo", "grupo");
+            
+            // IP de Raspberry
+            conexion = DriverManager.getConnection("jdbc:mysql://90.74.183.48/DAM03", "user_dam03", "grupo");
         } catch (ClassNotFoundException | SQLException e) {
             JOptionPane.showMessageDialog(null, "Error al conectar con la base de datos");
         }
